4f549cb15871894833eda3e82b9d90f7bc03e6b4
madelineoliver6 <madelineoliver6@gmail.com>
2025-07-14 15:54:08 -0400

    ToDoList set up

---
+++ productivity-tracker/src/components/ToDoList.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const ToDoList = () =>{
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+
+    function handleInputChange(event){
+        setNewTask(event.target.value);
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+
+    }
+
+    return(
+        <div className="to-do-list">
+          <h1>To-Do List</h1>  
+
+          <div>
+            <input
+                type="text"
+                placeholder="Enter A Task..."
+                value={newTask}
+                onChange={handleInputChange}
+            />
+            <button
+            className="add-button"
+            onClick={addTask}>
+                Add
+            </button>
+          </div>
+
+          <ol>
+            {tasks.map((task,index) =>
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button className="delete-button"
+                        onClick={()=>deleteTask(index)}>
+                        Delete
+                    </button>
+                </li>
+            )}
+          </ol>
+        </div>
+    );
+
+}
+
+export default ToDoList
